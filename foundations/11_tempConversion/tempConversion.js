18b7a015ca1d9e3eb8e080fc67cc7df0ef10ee70
LiR <linggeshraj0612@gmail.com>
2026-02-03 02:47:45 +0800

    11 Complete

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitVal) {
+  let celsiusVal = parseFloat(((fahrenheitVal-32)*(5/9)).toFixed(1));
+  console.log(celsiusVal);
+  let decPortion = parseFloat((celsiusVal%1).toFixed(1));
+  console.log(decPortion);
+  if(decPortion==0.0){
+    return Math.floor(celsiusVal);
+  } else {
+    return celsiusVal;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusVal) {
+  let fahrenheitVal = parseFloat(((9/5)*(celsiusVal)+32).toFixed(1));
+  let decPortion = parseFloat((fahrenheitVal%1).toFixed(1));
+  if(decPortion==0.0){
+    return Math.floor(fahrenheitVal);
+  } else {
+    return fahrenheitVal;
+  }
 };
 
 // Do not edit below this line
