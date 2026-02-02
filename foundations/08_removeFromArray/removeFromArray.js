fb003f4d566d712e85dcc19f70922aa9a84e27f5
LiR <linggeshraj0612@gmail.com>
2026-02-03 01:46:48 +0800

    08 Complete

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valsToRemove) {
+    const cleanedArr = arr.filter((value)=> !valsToRemove.includes(value));
+    return cleanedArr;
 };
 
 // Do not edit below this line
