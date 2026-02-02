4056584a537c4bdd450e5641fb76bfb619b80af1
LiR <linggeshraj0612@gmail.com>
2026-02-03 02:07:41 +0800

    09 Complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(firstVal, lastVal) {
+    if(firstVal>lastVal){
+        [firstVal, lastVal] = [lastVal, firstVal];
+    }
+    if(!Number.isInteger(firstVal) || !Number.isInteger(lastVal) || firstVal<0 ||lastVal<0){
+        return "ERROR"
+    }
+    totalSum = (lastVal*(lastVal+1))/2 - ((firstVal-1)*((firstVal-1)+1))/2
+    return totalSum;
 };
+
+//First choise is to create an array via loops then reduce()
 
 // Do not edit below this line
 module.exports = sumAll;
