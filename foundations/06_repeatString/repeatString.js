0d0ffd89b2f0be2dd23eb2d50e4551086ceb50bc
LiR <linggeshraj0612@gmail.com>
2026-02-02 23:45:04 +0800

    06 Complete

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num<0){
+        return 'ERROR';
+    }else{
+        retString = "";
+        for(let i=0; i<num; i++){
+            retString += string;
+        }
+        return retString;
+    }
+
 
 };
 
