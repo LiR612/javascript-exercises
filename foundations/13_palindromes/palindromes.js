e314ac3f4f2bb79ec479b5cf828ce256cf7c9571
LiR <linggeshraj0612@gmail.com>
2026-02-11 23:23:19 +0800

    13 Complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    const punctuationlessString = inputStr.replace(/[^a-zA-Z0-9\s]/g, "");
+    const finalStr = punctuationlessString.replace(/\s+/g, "").toLowerCase();
+    
+    let j = finalStr.length - 1;
+    for(let i=0; i<finalStr.length / 2; i++){
+        if (finalStr[i]!= finalStr[j]){
+            return false;
+        }
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
