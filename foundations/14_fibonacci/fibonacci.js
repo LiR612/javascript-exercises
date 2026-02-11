7061944ca66f61e1d5831db7dc4466eba0847641
LiR <linggeshraj0612@gmail.com>
2026-02-11 23:45:21 +0800

    14 Complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index<0)return "OOPS";
+    if(index==0)return 0;
+    let prev = 0;
+    let curr=1;
+    for(let i=2; i<=index; i++){
+        let next = curr + prev;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
