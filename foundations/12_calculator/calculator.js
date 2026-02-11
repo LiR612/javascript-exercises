f4c2dcefe73c7a531a800ab45f3897598a2766e0
LiR <linggeshraj0612@gmail.com>
2026-02-11 22:58:02 +0800

    12 Complete

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, item)=>
+    acc+item
+  ,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, item)=>
+    acc*item
+  ,1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let acc=1;
+	for(let i=1;i<=num;i++){
+    acc *= i;
+  }
+  return acc;
 };
 
 // Do not edit below this line
