1ebae9075b57d80b263baa9b17ca74bdefa9b5ae
LiR <linggeshraj0612@gmail.com>
2026-02-12 23:03:04 +0800

    16 Complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldest,person) =>{
+        let personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth
+        if(personAge>(oldest.yearOfDeath || currentYear) - oldest.yearOfBirth){
+            oldest = person;
+        }
+        return oldest;
+        
+    }, people[0])
 };
 
 // Do not edit below this line
